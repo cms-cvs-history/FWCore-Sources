d42d3d8d3eaf8e8f85aa772cc9cbfc6c830c7a75
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2010-06-18 19:31:36 +0000

    Synch with main trunk

--- interface/ExternalInputSource.h
+++ interface/ExternalInputSource.h
@@ -12,15 +12,19 @@
 #include "FWCore/Catalog/interface/InputFileCatalog.h"
 
 namespace edm {
+  class ParameterSet;
+  class ParameterSetDescription;
+
   class ExternalInputSource : public ConfigurableInputSource {
   public:
     ExternalInputSource(ParameterSet const& pset, InputSourceDescription const& desc, bool realData = true);
     virtual ~ExternalInputSource();
 
-  std::vector<std::string> const& logicalFileNames() const {return catalog_.logicalFileNames();}
-  std::vector<std::string> const& fileNames() const {return catalog_.fileNames();}
-  InputFileCatalog& catalog() {return catalog_;}
-
+    std::vector<std::string> const& logicalFileNames() const {return catalog_.logicalFileNames();}
+    std::vector<std::string> const& fileNames() const {return catalog_.fileNames();}
+    InputFileCatalog& catalog() {return catalog_;}
+    
+    static void fillDescription(ParameterSetDescription& desc);
 
   private:
     InputFileCatalog catalog_;
