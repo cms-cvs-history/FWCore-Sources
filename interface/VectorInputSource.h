04076b87b2c6170e390bda2f49d67233d312d3b5
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2011-05-24 22:12:18 +0000

    new interface for mixing modules

--- interface/VectorInputSource.h
+++ interface/VectorInputSource.h
@@ -24,18 +24,46 @@ namespace edm {
     explicit VectorInputSource(ParameterSet const& pset, InputSourceDescription const& desc);
     virtual ~VectorInputSource();
 
-    void readMany(int number, EventPrincipalVector& result);
-    void readManyRandom(int number, EventPrincipalVector& result, unsigned int& fileSeqNumber);
-    void readManySequential(int number, EventPrincipalVector& result, unsigned int& fileSeqNumber);
-    void readManySpecified(std::vector<EventID> const& events, EventPrincipalVector& result);
+    template<typename T>
+    void loopRandom(int number, T& eventOperator);
+    template<typename T>
+    void loopSequential(int number, T& eventOperator);
+    template<typename T>
+    void loopSpecified(std::vector<EventID> const& events, T& eventOperator);
+
     void dropUnwantedBranches(std::vector<std::string> const& wantedBranches);
 
   private:
-    virtual void readMany_(int number, EventPrincipalVector& result) = 0;
-    virtual void readManyRandom_(int number, EventPrincipalVector& result, unsigned int& fileSeqNumber) = 0;
-    virtual void readManySequential_(int number, EventPrincipalVector& result, unsigned int& fileSeqNumber) = 0;
-    virtual void readManySpecified_(std::vector<EventID> const& events, EventPrincipalVector& result) = 0;
+
+    virtual std::auto_ptr<EventPrincipal> readOneRandom() = 0;
+    virtual std::auto_ptr<EventPrincipal> readOneSequential() = 0;
+    virtual std::auto_ptr<EventPrincipal> readOneSpecified(EventID const& event) = 0;
+
     virtual void dropUnwantedBranches_(std::vector<std::string> const& wantedBranches) = 0;
   };
+
+  template<typename T>
+  void VectorInputSource::loopRandom(int number, T& eventOperator) {
+    for(int i = 0; i < number; ++i) {
+      std::auto_ptr<EventPrincipal> ep = readOneRandom();
+      eventOperator(*ep);
+    }
+  }
+
+  template<typename T>
+  void VectorInputSource::loopSequential(int number, T& eventOperator) {
+    for(int i = 0; i < number; ++i) {
+      std::auto_ptr<EventPrincipal> ep = readOneSequential();
+      eventOperator(*ep);
+    }
+  }
+
+  template<typename T>
+  void VectorInputSource::loopSpecified(std::vector<EventID> const& events, T& eventOperator) {
+    for(std::vector<EventID>::const_iterator it = events.begin(), itEnd = events.end(); it != itEnd; ++it) {
+      std::auto_ptr<EventPrincipal> ep = readOneSpecified(*it);
+      eventOperator(*ep);
+    }
+  }
 }
 #endif
